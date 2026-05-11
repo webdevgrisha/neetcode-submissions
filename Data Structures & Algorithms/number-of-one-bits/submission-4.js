fced65993fa3e22680504830bbe073e7edc6f88f
Ryhor Roi <36422717+webdevgrisha@users.noreply.github.com>
2026-05-11 20:29:03 +0200

    Add: number-of-one-bits - submission-4

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-4.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number} n - a positive integer
+     * @return {number}
+     */
+    hammingWeight(n) {
+        let count = 0;
+
+        while(n > 0) {
+            if((n & 1) === 1) count++;
+
+            n = (n >> 1);
+        }
+
+        return count;
+    }
+}
