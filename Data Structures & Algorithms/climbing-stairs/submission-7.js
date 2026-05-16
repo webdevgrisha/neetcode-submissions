b67e1dd9983f235453290432efdccc1e22637c26
Ryhor Roi <36422717+webdevgrisha@users.noreply.github.com>
2026-05-16 17:04:07 +0200

    Add: climbing-stairs - submission-7

---
+++ Data Structures & Algorithms/climbing-stairs/submission-7.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n, cache = new Map()) {
+        if (n <= 3) return n;
+
+        if (cache.has(n)) return cache.get(n);
+
+        cache.set(n, this.climbStairs(n - 1, cache) + this.climbStairs(n - 2, cache));
+
+        return cache.get(n);
+    }
+}
