eda1837ce9b42a88229cbb94095065ed94511312
Ryhor Roi <36422717+webdevgrisha@users.noreply.github.com>
2026-05-20 01:35:48 +0200

    Add: replace-elements-with-greatest-element-on-right-side - submission-0

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number[]}
+     */
+    replaceElements(arr) {
+        let currMax = -1;
+
+        for (let i = arr.length - 1; i >= 0; i--) {
+            const currVal = arr[i];
+
+            arr[i] = currMax;
+            currMax = Math.max(currMax, currVal);
+        }
+
+        return arr;
+    }
+}
