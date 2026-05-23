dcd01b59277216751feefd1c07958b0cbc613f63
Ryhor Roi <36422717+webdevgrisha@users.noreply.github.com>
2026-05-24 01:41:28 +0200

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        let p1 = 0;
+
+        for (let num of nums) {
+            if (num === val) continue;
+
+            nums[p1] = num;
+            p1++;
+        }
+
+        return p1;
+    }
+}
