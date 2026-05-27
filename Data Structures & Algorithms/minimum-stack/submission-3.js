f4d6fdeadd9d63ba00385a096bf23642e03ca34a
Ryhor Roi <36422717+webdevgrisha@users.noreply.github.com>
2026-05-27 10:57:50 +0200

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.js
@@ -0,0 +1,40 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+
+        const minVal = Math.min(val, this.minStack.at(-1) ?? val);
+
+        this.minStack.push(minVal);
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack.at(-1);
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minStack.at(-1);
+    }
+}
