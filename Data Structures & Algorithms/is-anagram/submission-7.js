c9de560549c1896b68178b47d682878acd0588ec
Ryhor Roi <36422717+webdevgrisha@users.noreply.github.com>
2026-05-29 18:17:50 +0200

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) return false;
+
+        const charsCount = new Array(26).fill(0);
+
+        for (let i = 0; i < s.length; i++) {
+            const sCharPos = s[i].charCodeAt(0) - "a".charCodeAt(0);
+            const tCharPos = t[i].charCodeAt(0) - "a".charCodeAt(0);
+
+            charsCount[sCharPos]++;
+            charsCount[tCharPos]--;
+        }
+
+        return charsCount.every((pos) => !pos);
+    }
+}
