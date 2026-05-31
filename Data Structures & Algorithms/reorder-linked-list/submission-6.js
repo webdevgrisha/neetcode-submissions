11aefa0e8e63796102f202fb37024be0bbc174f5
Ryhor Roi <36422717+webdevgrisha@users.noreply.github.com>
2026-05-31 15:22:01 +0200

    Add: reorder-linked-list - submission-6

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-6.js
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {void}
+     */
+    reorderList(head) {
+        const secondHalf = this.cutHalf(head);
+        const reversedHalf = this.reverse(secondHalf);
+        
+        this.merge(head, reversedHalf);
+        
+        return head;
+    }
+
+    cutHalf(head) {
+        let slow = head;
+        let fast = head.next;
+
+        while (fast && fast.next) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        const second = slow.next;
+        slow.next = null; 
+        
+        return second;
+    }
+    reverse(head) {
+        let prev = null;
+        let curr = head;
+
+        while (curr) {
+            const next = curr.next;
+
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+    merge(list1, list2) {
+        while (list1 && list2) {
+            let [list1Next, list2Next] = [list1.next, list2.next];
+
+            list1.next = list2;
+            list2.next = list1Next;
+
+            list1 = list1Next;
+            list2 = list2Next;
+        }
+    }
+}
