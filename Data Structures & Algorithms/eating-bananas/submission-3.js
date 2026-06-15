190c30ba71ad33ca97cd7762cc50273e56795431
Ryhor Roi <36422717+webdevgrisha@users.noreply.github.com>
2026-06-15 21:01:38 +0200

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let speed = 1;
+        while (true) {
+            let totalTime = 0;
+            for (let pile of piles) {
+                totalTime += Math.ceil(pile / speed);
+            }
+
+            if (totalTime <= h) {
+                return speed;
+            }
+            speed++;
+        }
+    }
+}
