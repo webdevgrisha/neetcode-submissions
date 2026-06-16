581f8f25336a687440fcb1caa392bd8dee6adc61
Ryhor Roi <36422717+webdevgrisha@users.noreply.github.com>
2026-06-16 21:04:57 +0200

    Add: insert-into-a-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/insert-into-a-binary-search-tree/submission-0.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {number} val
+     * @return {TreeNode}
+     */
+    insertIntoBST(root, val) {
+        if (!root) return new TreeNode(val);
+
+        if (val > root.val) {
+            root.right = this.insertIntoBST(root.right, val);
+        } else {
+            root.left = this.insertIntoBST(root.left, val);
+        }
+
+        return root;
+    }
+}
