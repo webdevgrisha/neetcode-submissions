34a763dc1627709d071293d45497871182e9bfd2
Ryhor Roi <36422717+webdevgrisha@users.noreply.github.com>
2026-06-20 00:12:49 +0200

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    subsets(nums) {
+        const result = [];
+        const subset = [];
+
+        function dfs(i) {
+            if (i >= nums.length) {
+                result.push([...subset]);
+                return;
+            }
+
+            subset.push(nums[i]);
+            dfs(i + 1);
+
+            subset.pop();
+            dfs(i + 1);
+        }
+
+        dfs(0);
+
+        return result;
+    }
+}
