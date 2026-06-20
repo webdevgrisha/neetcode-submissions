27996822d86ac8e14c78436dba0bfb2224313f88
Ryhor Roi <36422717+webdevgrisha@users.noreply.github.com>
2026-06-20 16:42:11 +0200

    Add: kth-largest-integer-in-a-stream - submission-5

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-5.js
@@ -0,0 +1,34 @@
+class KthLargest {
+    /**
+     * @param {number} k
+     * @param {number[]} nums
+     */
+    constructor(k, nums) {
+        this.minHeap = new MinPriorityQueue();
+        this.k = k;
+
+        for (const num of nums) {
+            this.minHeap.enqueue(num);
+
+            if (this.minHeap.size() > k) {
+                this.minHeap.dequeue();
+            }
+        }
+
+        // while (this.minHeap.size() > k) {}
+    }
+
+    /**
+     * @param {number} val
+     * @return {number}
+     */
+    add(val) {
+        this.minHeap.enqueue(val);
+
+        if (this.minHeap.size() > this.k) {
+            this.minHeap.dequeue();
+        }
+
+        return this.minHeap.front();
+    }
+}
